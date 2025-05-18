0b8436750d35ca4d783a3726e634b27d84c049c4
Sophomores <1404462714@qq.com>
2025-05-18 16:56:25 +0800

    更新时间: 2025-05-18-16-54

--- 链表/2_设计链表_用size_力扣序号_206.cpp
+++
@@ -1,8 +0,0 @@
-struct LinkNode {
-    int val;
-    struct LinkNode *next;
-    // 构造函数体
-    LinkNode() : val(0), next(nullptr) {}
-    LinkNode(int x) : val(x), next(nullptr) {}
-    LinkNode(int x, LinkNode *node) : val(x), next(node) {}
-};
